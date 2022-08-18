c0de9b91871ff5cc18190cbe04e291d4f0767f7b
Luis <noudox@gmail.com>
2022-08-18 23:39:14 +0200

    all10

---
+++ nombre_venv/src/complejidad_algoritmica.py
@@ -0,0 +1,32 @@
+import time
+
+#implementación iterativa 
+
+def factorial(n):
+    respuesta = 1
+    while n>1:
+        respuesta *=n 
+        n -= 1
+    return respuesta
+
+#implementación recursivo 
+
+def factorial_r(n):
+    if n == 1:
+        return 1
+    
+    return n * factorial_r(n - 1)
+
+if __name__ == "__main__":
+    n = 200000
+
+    comienzo = time.time()
+    print(factorial(n))
+    final = time.time()
+    print(final - comienzo)
+
+
+    comienzo_r = time.time()
+    print(factorial_r(n))
+    final_r = time.time()
+    print(final_r - comienzo_r)
